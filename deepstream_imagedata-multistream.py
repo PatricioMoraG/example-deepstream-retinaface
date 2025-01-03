33372f872dfba9bb847532580ad0a97b82f3f118
patricio.mora <patricio.mora@spotcloud.io>
2025-01-03 10:20:38 -0300

    fix config

--- deepstream_imagedata-multistream.py
+++ deepstream_imagedata-multistream.py
@@ -353,7 +353,7 @@ def main(args):
     streammux.set_property('batch-size', number_sources)
     streammux.set_property('batched-push-timeout', 4000000)
     #pgie.set_property('config-file-path', "dstest_imagedata_config.txt")
-    pgie.set_property('config-file-path', "config_infer_retinaface.txt")
+    pgie.set_property('config-file-path', "retinaface_config.txt")
     pgie_batch_size = pgie.get_property("batch-size")
     if (pgie_batch_size != number_sources):
         print("WARNING: Overriding infer-config batch-size", pgie_batch_size, " with number of sources ",
